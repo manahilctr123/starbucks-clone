f72b2d7341dae5660083d6815fdfdcc936346504
Manahil <manahilfaheem120@gmail.com>
2025-01-22 23:52:28 +0400

    Comment out unused Link import in Home page for code clarity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 // import { motion } from "framer-motion";
-import Link from "next/link";
+// import Link from "next/link";
 import Image from "next/image";
 // import FacebookIcon from "../components/FacebookIcon";
 // import InstagramIcon from "../components/InstagramIcon";
